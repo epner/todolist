14658301d9f0e40926f7bcc7a5b7ca3a88d671d5
Kristo Epner <kristoepner@hot.ee>
2017-08-24 13:48:46 +0300

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 	// Create a "close" button and append it to each list item
 $(document).ready(
 		function() {
@@ -45,30 +44,3 @@ $(document).ready(
 			});
 
 });
-=======
-	// Create a "close" button and append it to each list item
-$(document).ready(
-		function() {
-			$("#button").click(
-					function() {
-        		var toAdd = $("input[name=checkListItem]").val();
-        			$(".list").append('<li>' + ' ' + toAdd + '</li>');
-    			});
-	
-					var list = document.querySelector('.list');
-						list.addEventListener('click', 
-							function(ev) {
-								if(ev.target.tagName === 'LI') {
-									ev.target.classList.toggle('done'); 
-								}
-							}, false);
-			
-			$("input[name=checkListItem]").keyup(function(event){
-				if(event.keyCode == 13){
-					$("#button").click();
-				}
-			});
-			
-			$('.list').sortable();
-		});
->>>>>>> f276f690e79ceb0b679f3d27dc2926a79e8c61ef
