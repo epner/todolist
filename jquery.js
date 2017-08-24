4f0e85a5e6edf4557ff7f89f3c5879a6b505517d
epner <kristoepner@hot.ee>
2017-08-24 13:37:38 +0300

    changed

---
+++ jquery.js
@@ -0,0 +1,46 @@
+	// Create a "close" button and append it to each list item
+$(document).ready(
+		function() {
+			$("#button").click(
+					function() {
+        		var toAdd = $("input[name=checkListItem]").val();
+        			$(".list").append('<li>' + ' ' + toAdd + '</li>');
+    			});
+
+					var list = document.querySelector('.list');
+						list.addEventListener('click',
+							function(ev) {
+								if(ev.target.tagName === 'LI') {
+									ev.target.classList.toggle('done');
+								}
+							}, false);
+
+			$("input[name=checkListItem]").keyup(function(event){
+				if(event.keyCode == 13){
+					$("#button").click();
+				}
+			});
+
+			$('.list').sortable();
+
+
+			$(function() {
+			// select all desired input fields and attach tooltips to them
+					 $("#toDoForm :input").tooltip({
+
+			// place tooltip on the right edge
+					 position: "center right",
+
+			// a little tweaking of the position
+					 offset: [-2, 10],
+
+			// use the built-in fadeIn/fadeOut effect
+					 effect: "fade",
+
+			// custom opacity setting
+					 opacity: 0.7
+
+					 });
+			});
+
+});
